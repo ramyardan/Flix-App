e59e53b698b2ed9d7e8bf90335753292b08f0988
ramyardan <90062480+ramyardan@users.noreply.github.com>
2022-02-10 20:07:31 -0500

    Added Part 2 of Flix App

---
+++ FlixPart2/Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Ramyar Daneshgar on 2/8/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
